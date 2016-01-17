61e5bd553e575454610601a543ee11b899f0e961
Nikita <keddofilatov@gmail.com>
2016-01-17 15:05:25 +0600

    add test for invalid img_url

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -36,6 +36,10 @@ class ProductTest < ActiveSupport::TestCase
     ok.each do |name|
       assert new_product(name).valid?, "#{name} shouldn`t be invalid`"
     end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn`t be valid`"
+    end
   end
 
   def new_product(image_url)
