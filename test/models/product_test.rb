e986181bc23151ec91c0fc439192ad17cc79c818
Nikita <keddofilatov@gmail.com>
2016-01-17 15:04:04 +0600

    add test for valid img_url

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -28,4 +28,22 @@ class ProductTest < ActiveSupport::TestCase
     product.price= 1
     assert product.valid?
   end
+
+  test "image url"do
+    ok = %w{fred.gif fred.jpg fred.png}
+    bad = %w{fred.doc fred.gif/more}
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn`t be invalid`"
+    end
+  end
+
+  def new_product(image_url)
+    Product.new(
+               title: "Title",
+               description: "Description",
+               price: 13.99,
+               image_url: image_url
+    )
+  end
 end
