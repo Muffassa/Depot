e4c7b03280884f61b85d5fbd28322a4c34e35acd
Nikita <keddofilatov@gmail.com>
2016-01-19 00:48:53 +0600

    Add down method for combine items migration

--- db/migrate/20160118183042_combine_items_in_cart.rb
+++ db/migrate/20160118183042_combine_items_in_cart.rb
@@ -19,6 +19,12 @@ class CombineItemsInCart < ActiveRecord::Migration
   end
 
   def down
-
+    LineItem.where("quantity>1").each do |line_item|
+      line_item.quantity.times do
+        LineItem.create cart_id: line_item.cart_id,
+            product_id: line_item.product_id, quantity: 1
+      end
+      line_item.destroyr
+    end
   end
 end
